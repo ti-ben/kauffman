670f55a8ddc47b94ee1d4b8be89368d3542789fc
ti-ben <benoit.titeux@protonmail.com>
2022-06-23 23:58:38 +0200

    New project

---
+++ backend/src/test/java/com/kauffman/management/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauffman.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
